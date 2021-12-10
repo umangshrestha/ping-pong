3e1526db022aee08935399d8d4fa53eee579b2b3
umang <umangshrestha09@gmail.com>
2021-12-10 14:45:25 +0545

    Updates

---
+++ precache-manifest.e15e01d65df35a33b04313a2ff8dfd1b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95ac22f3550e6f0320bf544e094834ba",
+    "url": "/gh-pages/index.html"
+  },
+  {
+    "revision": "3f67475261bd4d59c558",
+    "url": "/gh-pages/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "6ddea1032046f09c68c8",
+    "url": "/gh-pages/static/js/2.ce74096c.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/gh-pages/static/js/2.ce74096c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46e5f63843f030a7a54c",
+    "url": "/gh-pages/static/js/3.3a344492.chunk.js"
+  },
+  {
+    "revision": "3f67475261bd4d59c558",
+    "url": "/gh-pages/static/js/main.2a5d0873.chunk.js"
+  },
+  {
+    "revision": "6944b7b9ab800f515c55",
+    "url": "/gh-pages/static/js/runtime-main.28aea91b.js"
+  }
+]);
