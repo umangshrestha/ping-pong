8a129003f567f02a4f3318f130ae3d6fb6205067
umang <umangshrestha09@gmail.com>
2021-12-10 17:46:32 +0545

    Updates

---
+++ precache-manifest.e10d12865b914750d8af50ddfde402db.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca4c5b7267f385ea0247be9bf199a161",
+    "url": "/ping-pong/index.html"
+  },
+  {
+    "revision": "2f560686053d02996df9",
+    "url": "/ping-pong/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "6ddea1032046f09c68c8",
+    "url": "/ping-pong/static/js/2.ce74096c.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/ping-pong/static/js/2.ce74096c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46e5f63843f030a7a54c",
+    "url": "/ping-pong/static/js/3.3a344492.chunk.js"
+  },
+  {
+    "revision": "2f560686053d02996df9",
+    "url": "/ping-pong/static/js/main.d7eb9222.chunk.js"
+  },
+  {
+    "revision": "27d5b2e8c0e5e190cf33",
+    "url": "/ping-pong/static/js/runtime-main.b5a50a74.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/gh-pages/precache-manifest.2849f541f7a226243e7b25006776611f.js"
+  "/ping-pong/precache-manifest.e10d12865b914750d8af50ddfde402db.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/gh-pages/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ping-pong/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
